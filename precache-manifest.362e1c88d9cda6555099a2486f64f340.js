ab0416682aceb59d9fc8a5349dc8bcdfae51c46f
Rahul Sahu <therahulsahu7@gmail.com>
2021-10-19 20:17:09 +0530

    Updates

---
+++ precache-manifest.362e1c88d9cda6555099a2486f64f340.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc36b8df87575f9f7ef45210724328b",
+    "url": "/React-Color-Palette/index.html"
+  },
+  {
+    "revision": "a90dfe82b13adb2ede8a",
+    "url": "/React-Color-Palette/static/css/2.0da342d8.chunk.css"
+  },
+  {
+    "revision": "5c251ab15556852c9102",
+    "url": "/React-Color-Palette/static/css/main.bbac7fe8.chunk.css"
+  },
+  {
+    "revision": "a90dfe82b13adb2ede8a",
+    "url": "/React-Color-Palette/static/js/2.ab61e374.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/React-Color-Palette/static/js/2.ab61e374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c251ab15556852c9102",
+    "url": "/React-Color-Palette/static/js/main.c2f14a24.chunk.js"
+  },
+  {
+    "revision": "4ad3e41b707510232fea",
+    "url": "/React-Color-Palette/static/js/runtime-main.d5138b72.js"
+  },
+  {
+    "revision": "777140b04e6de99640d7bb65cd4ae211",
+    "url": "/React-Color-Palette/static/media/bg.777140b0.svg"
+  }
+]);
